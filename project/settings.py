24f774cdcda67f30eb8f4acf3b3e96d87b8a861d
ahmedshahin2000 <42018024@hti.edu.eg>
2022-02-11 17:40:24 +0200

    change domain name

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = config('DEBUG')
-ALLOWED_HOSTS = ['127.0.0.1','localhost','mri-detection-cancer-project.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','mri-detection-graduateproject.herokuapp.com']
 
 # ALLOWED_HOSTS = []
 
